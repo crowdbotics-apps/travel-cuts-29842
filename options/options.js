fcab0e031c9adc4f0bb932dd8c1f17605aaced43
Crowdbotics <team@crowdbotics.com>
2021-08-16 02:19:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "travel_cuts_29842Identifier",
+  url: "https://travel_cuts_29842Identifier.botics.co",
+  api: "https://travel_cuts_29842Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
